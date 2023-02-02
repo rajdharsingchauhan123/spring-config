c78f051a413061f1957f4efab7c3cecae5aa9e03
raj <raj@gmail.com>
2023-02-02 15:52:04 +0530

    check the two number

---
+++ Downloads/Dailycoding/src/main/java/org/example/Main.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        int []arr={1,10,3,6,7};
+
+
+        System.out.println("Hello world!");
+        List<Integer>list= Arrays.asList(1222,5,65);
+        Collections.sort(list);
+        System.out.println(list);
+        System.out.println(list.get(1));
+
+
+    }
+}
---
+++ Downloads/Dailycoding/src/main/java/org/example/javapep/Demo1.java
@@ -0,0 +1,19 @@
+package org.example.javapep;
+
+import java.util.Scanner;
+
+public class Demo1 {
+    public static void main(String[] args) {
+        int a = 12;
+        int b = 15;
+
+
+        if (a < b) {
+            System.out.println("print a in less than b" + a);
+        } else {
+
+
+            System.out.println("other wise b is greaet" + b);
+        }
+    }
+}
