abc61d9da15506ac0f717165fc86d09ddabc6594
raj <raj@gmail.com>
2023-04-07 18:56:36 +0530

    add some value

--- Downloads/javapractise/src/main/java/org/example/javapractise/Person.java
+++ Downloads/javapractise/src/main/java/org/example/javapractise/Person.java
@@ -1,23 +1,25 @@
 package org.example.javapractise;
 
 public class Person {
-    int a=20;
-    static int b=50;
-     volatile  int c=14;
-}
-class Child extends Person{
-//    int a=20;
-// int b=50;
-//    volatile int c=14;
 
     public static void main(String[] args) {
-//        Person c =new Child();
-//        System.out.println(c.a);
-//        System.out.println(c.b);
-//        System.out.println(c.c);
-        System.out.println(b);
-//        System.out.println(c);
+        System.out.println("hello world");
+        System.out.println("hello"+1);
+        System.out.println(1+1+"hello");
+        System.out.println(1!=2);
+        System.out.println(2==2);
+        System.out.println("hello"+1==3+"jelloikjdkldj;f");
+        System.out.println(2^3);
+        System.out.println();
+
+
+
+
     }
+
+
+
+
 
 }
 
